2d893290c77d48330e00e953d5d7d14fdf4fae82
2425403084 <2425403084@live.gctu.edu.gh>
2026-05-31 19:02:49 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,22 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Kotei John Stephen Ashie
+# STUDENT ID: 2425403084
+# Email: 2425403084@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, I Kotei John Stephen Ashie, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
+    print("...JJJJJJJJJJ.......AAAAA.........SSSSSSSSS.........K......K")
+    print(".......JJ..........AA...AA.......SS.................K....K")
+    print(".......JJ.........AA.....AA.......SS................K..K")
+    print(".......JJ.........AAAAAAAAA........SSSSSSS..........KKK")
+    print(".......JJ.........AA.....AA..............SS.........K..K")
+    print("JJ.....JJ.........AA.....AA...............SS........K...K")
+    print(".JJJJJJ...........AA.....AA......SSSSSSSSS..........K......K")
 
 main()
